cee5bc7b4eaeb4a8a7d4f6dd8cfae63d7703492d
serbanesculeonard95 <serbanescu.leonard95@gmail.com>
2026-06-26 15:11:27 +0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://jfosomkclvsfsmnhgzyj.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_5-9xPCRaVI1P50kzN80FSg_lEV03...'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
