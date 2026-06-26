929fbeeb5974323f5e694e7a30a5b7a479788bc6
serbanesculeonard95 <serbanescu.leonard95@gmail.com>
2026-06-26 15:00:45 +0300

    Delete src/src directory

--- src/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://jfosomkclvsfsmnhgzyj.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_5-9xPCRaVI1P50kzN80FSg_lEVO3xMf'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
